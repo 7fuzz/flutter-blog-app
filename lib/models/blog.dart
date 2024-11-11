8e7cf89610866005dd537af2dda385dfcf6774b8
7fuzz <155941293+7fuzz@users.noreply.github.com>
2024-11-11 20:10:56 +0700

    dashboard add blog list
    eeeeeeeeeeeeeeeeeeeee

--- lib/models/blog.dart
+++ lib/models/blog.dart
@@ -9,6 +9,7 @@ class Blog {
   String? body; // isi blog
   DateTime? published; // tanggal rilis blog
   User? writer; // Penulis
+  String? banner; // Gambar banner atau sampul, sementara dari local
 
   
   Blog({
@@ -16,7 +17,8 @@ class Blog {
     this.title,
     this.body,
     this.published,
-    this.writer
+    this.writer,
+    this.banner
   });
   
   // membuat id random
@@ -33,34 +35,38 @@ class Blog {
   }
 
   // List blog pengganti database
-  List<Blog> blogs = [
+  static List<Blog> blogs = [
     Blog(
       id: _generateRandomId(),
       title: 'UTB: Teknik Informatika',
       body: 'Bersama Teknik Informatika Sekolah Tinggi Teknologi Bandung menghadirkan generasi yang melek terhadap teknologi dan berani berinovasi kreatif sehingga lahirlah generasi yang “Mampu Menganalisa, Merancang, dan Membangun App & Perangkat Komputer, paham terhadap Information Tech Development, Comp and Network Security, Robotics and Embedded System',
       published: DateTime(2024, 4, 23),
-      writer: User.getUser('admin')
+      writer: User.getUser('admin'),
+      banner: 'assets/banner/tif.jpg'
     ),
     Blog(
       id: _generateRandomId(),
       title: 'Mengapa Teknik Informatika Menawarkan Peluang Karir yang Menjanjikan di Era Digital? ',
       body: 'Di tengah perkembangan pesat teknologi informasi, program studi Teknik Informatika menjadi salah satu jurusan yang semakin diminati oleh calon mahasiswa yang ingin berkarir di bidang teknologi. Di Universitas Teknologi Bandung (UTB), program studi ini dirancang untuk memberikan pengetahuan dan keterampilan yang dibutuhkan untuk memahami dan memecahkan berbagai tantangan di dunia teknologi informasi, komputasi, dan pengembangan perangkat lunak. Lulusan program studi ini nantinya akan menyandang gelar Sarjana Komputer (S.Kom), sebuah gelar yang diakui secara nasional dan internasional, yang membuka berbagai peluang karir di industri teknologi yang berkembang pesat.',
       published: DateTime(2024, 6, 22),
-      writer: User.getUser('staff')
+      writer: User.getUser('staff'),
+      banner: 'assets/banner/himatif.jpg'
     ),
     Blog(
       id: _generateRandomId(),
       title: 'Rice Disease: Solusi Teknologi dari Mahasiswa UTB untuk Petani Dalam Meningkatkan Ketahanan Pangan',
       body: 'Kevin Christianto, mahasiswa Universitas Teknologi Bandung (UTB), berhasil ciptakan sebuah aplikasi berbasis Android bernama Rice Disease Detection. Aplikasi ini bertujuan membantu petani padi dalam mendeteksi penyakit pada tanaman padi serta memberikan panduan cara penanganannya. Inovasi ini hadir sebagai jawaban atas tantangan yang sering dihadapi para petani, yaitu kesulitan dalam mengenali penyakit yang menyerang tanaman padi secara dini.',
       published: DateTime(2024, 7, 2),
-      writer: User.getUser('mahasiswa')
+      writer: User.getUser('mahasiswa'),
+      banner: 'assets/banner/rice.jpg'
     ),
     Blog(
       id: _generateRandomId(),
       title: 'Rahasia Menggunakan Seele di Honkai Star Rail untuk Serangan Mematikan!',
       body: 'Seele adalah karakter 5-star dengan kemampuan serangan Quantum yang mematikan. Untuk memaksimalkan potensinya, manfaatkan Skill "Night Shine" untuk meningkatkan serangan dan Ultimate "Starfall" yang memberikan damage besar serta serangan lanjutan. Fokuskan pada relik yang meningkatkan Crit Rate dan Crit Damage, serta gunakan karakter pendukung yang bisa meningkatkan crit damage dan melakukan menambah kecepatan Seele seperti Bronya atau Sparkle, agar Seele bisa memberikan damage maksimal di setiap pertarungan!',
       published: DateTime(2024, 8, 13),
-      writer: User.getUser('yusril')
+      writer: User.getUser('yusril'),
+      banner: 'assets/banner/seele.jpg'
     ),
   ];
 
--- lib/views/dashboard.dart
+++ lib/views/dashboard.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:uts_pm2/models/blog.dart';
 import 'package:uts_pm2/models/user.dart';
 
 class DashboardScreen extends StatelessWidget {
@@ -10,18 +11,139 @@ class DashboardScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: const Text('Dashboard')),
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Text('Username: ${user.username}'),
-            Text('Name: ${user.name}'),
-            Text('Email: ${user.email}'),
-            Text('Phone: ${user.phone}'),
-          ],
-        ),
+      body: Column(
+        children: [
+          // Gridview untuk tombol
+          GridView.count(
+            shrinkWrap: true,
+            physics: const NeverScrollableScrollPhysics(),
+            padding: const EdgeInsets.all(10),
+            crossAxisCount: 4,
+            mainAxisSpacing: 10,
+            crossAxisSpacing: 10,
+            children: [
+              _buildDashboardButton(
+                icon: Icons.person,
+                label: 'Profile',
+                onPressed: () {},
+              ),
+              _buildDashboardButton(
+                icon: Icons.settings,
+                label: 'Setting',
+                onPressed: () {},
+              ),
+              _buildDashboardButton(
+                icon: Icons.article,
+                label: 'My blog',
+                onPressed: () {},
+              ),
+              _buildDashboardButton(
+                icon: Icons.edit,
+                label: 'Write blog',
+                onPressed: () {},
+              ),
+            ],
+          ),
+          
+          const Divider(
+            color: Colors.grey,
+            thickness: 10,
+            height: 10,
+          ),
+          // ListView untuk list blog
+          Expanded(
+            child: ListView.builder(
+              padding: const EdgeInsets.all(10),
+              itemCount: Blog.blogs.length,
+              itemBuilder: (context, index) {
+                final blog = Blog.blogs[index];
+                
+                // Memotong teks isi jadi satu baris
+                final truncatedBody = (blog.body != null && blog.body!.length > 50)
+                    ? '${blog.body!.substring(0, 47)}...'
+                    : blog.body ?? 'Tidak ada isi'; // Jika body null
+
+                return Card(
+                  margin: const EdgeInsets.symmetric(vertical: 8, horizontal: 5),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      // Menampilkan banner gambar
+                      if (blog.banner != null)
+                        Image.asset(blog.banner!, fit: BoxFit.cover, width: double.infinity, height: 150),
+                      Padding(
+                        padding: const EdgeInsets.all(10),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                              blog.title ?? 'Tidak ada judul', // Jika title null
+                              style: const TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+                            ),
+                            const SizedBox(height: 4),
+                            Text(
+                              // Waktu terbit sesuai dengan zona waktu
+                              'Diterbitkan: ${blog.published?.toLocal().toString().split(' ')[0]}',
+                              style: const TextStyle(fontSize: 12, color: Colors.grey),
+                            ),
+                            const SizedBox(height: 4),
+                            Text(
+                              'Penulis: ${blog.writer?.name ?? 'Tidak ada penulis'}',
+                              style: const TextStyle(fontSize: 12, color: Colors.grey),
+                            ),
+                            const SizedBox(height: 8),
+                            Text(
+                              truncatedBody,
+                              maxLines: 1,
+                              overflow: TextOverflow.ellipsis,
+                              style: const TextStyle(fontSize: 14),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ],
+                  ),
+                );
+              },
+            ),
+          ),
+        ],
       ),
+    );
+  }
+
+  // widget untuk membuat tombol
+  Widget _buildDashboardButton({
+    required IconData icon,
+    required String label,
+    required VoidCallback onPressed,
+    }) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        GestureDetector(
+          onTap: onPressed,
+          child: Container(
+            decoration: BoxDecoration(
+              shape: BoxShape.circle,
+              color: Colors.black.withOpacity(0.1), 
+            ),
+            padding: const EdgeInsets.all(12), 
+            child: Icon(
+              icon,
+              size: 24,
+              color: Colors.black,
+            ),
+          ),
+        ),
+        const SizedBox(height: 6),
+        Text(
+          label,
+          style: const TextStyle(
+            fontSize: 10,
+          ),
+        ),
+      ],
     );
   }
 }
